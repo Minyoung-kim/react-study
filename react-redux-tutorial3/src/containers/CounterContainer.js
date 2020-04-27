650763d19088da6db62bb42f275ac7681e6bfdec
mykim929 <mykim929@nexon.co.kr>
2020-04-27 15:33:32 +0900

    17. redux - hooks를 사용하여 컨테이너 컴포넌트 만들기

--- react-redux-tutorial3/src/containers/CounterContainer.js
+++ react-redux-tutorial3/src/containers/CounterContainer.js
@@ -1,21 +1,47 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import Counter from '../components/Counter';
-import { increase, decrease } from '../modules/counter';
+// import React from 'react';
+// import { connect } from 'react-redux';
+// import Counter from '../components/Counter';
+// import { increase, decrease } from '../modules/counter';
 
-const CounterContainer = ({ number, increase, decrease }) => {
-    return <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+// const CounterContainer = ({ number, increase, decrease }) => {
+//     return <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+// };
+
+// export default connect(
+//     state => ({
+//         number: state.counter.number,
+//     }),
+//     {
+//         increase,
+//         decrease,
+//     }
+// )(CounterContainer);
+
+
+// useDispatch를 사용할때에는 useCallback과 함께 사용하는 것이 좋음.
+// useStore 를 사용하면 컴포넌트 내부에서 리덕스 스토어 객체를 직접사용할 수 있음.
+// useStore는 컴포넌트에서 어쩌다가 스토어에 직접 접근해야하는 상황에서만 사용해야함.
+
+import React, { useCallback } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import Counter from '../components/Counter';
+import  { increase, decrease } from '../modules/counter';
+
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number);
+    const dispatch = useDispatch();
+    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+    const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+    return (
+        <Counter
+            number = {number}
+            onIncrease = {onIncrease}
+            onDecrease = {onDecrease}
+        />
+    )
 };
 
-export default connect(
-    state => ({
-        number: state.counter.number,
-    }),
-    {
-        increase,
-        decrease,
-    }
-)(CounterContainer);
+export default CounterContainer;
 
 // 위 컴포넌트를 리덕스와 연동하려면 connect 함수를 사용해야함.
 // connect (mapStateToProps, mapDispatchToProps)(연동할 컴포넌트)
